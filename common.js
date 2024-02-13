4273d1ff9109da530ba65dfcd9921a88e3db476e
Norman Rasmussen <norman@rasmussen.co.za>
2024-02-12 19:45:00 -0800

    Add Web UI

---
+++ common.js
@@ -0,0 +1,80 @@
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  if(0) {
+  navigator.serviceWorker.register('worker.js').then(function(registration) {
+    console.log('Service worker registration succeeded: ' + (registration.waiting ? "waiting to update..." : "no update"));
+  }, /*catch*/ function(error) {
+    console.error('Service worker registration failed:', error);
+  });
+  }
+} else {
+  console.warn('Service workers are not supported.');
+}
+
+let touchstart = async (event) => {
+  let button = event.target.closest('button');
+  if (!button) return;
+  event.preventDefault();
+  console.log(event.type, button.dataset.keycode)
+  let data = {
+    "raw": button.dataset.raw,
+  };
+  let led = document.getElementById('led');
+  led.style = "color:red";
+  let query = Object.entries(data).map(([k, v]) => escape(k) + '=' + escape(v)).join('&');
+  const request = new Request("/sendir?" + query, {
+    method: 'POST',
+    headers: {
+      'Accept': 'text/plain',
+    },
+  })
+  let response;
+  try {
+    response = await fetch(request);
+  } catch (err) {
+    led.style = "";
+    alert("Error trying to connect to server: " + err);
+    return;
+  }
+  if (response.status == 401 /* Unauthorized */) {
+    data.redirect = location.pathname;
+    let query = Object.entries(data).map(([k, v]) => escape(k) + '=' + escape(v)).join('&');
+    const f = document.createElement('form');
+    f.style.display = 'none';
+    f.method = 'POST';
+    f.action = "/sendir?" + query;
+    document.body.appendChild(f);
+    f.submit();
+    return;
+  }
+  led.style = "";
+  response_text = await response.text();
+  msg = response.status + " " + response.statusText + ":\n" + response_text;
+  if (response.status >= 200 && response.status < 300) {
+    if (response.status > 200 && response_text) {
+      console.warn("POST " + response.url + " " + msg);
+      alert(msg);
+    }
+  } else {
+    alert(msg);
+  }
+};
+let touchend = (event) => {
+  let button = event.target.closest('button');
+  if (!button) return;
+  event.preventDefault();
+  console.log(event.type, button.dataset.keycode)
+};
+let buttondown = (event) => {
+  if (event.button == 0) touchstart(event)
+}
+let buttonup = (event) => {
+  if (event.button == 0) touchend(event)
+}
+[...document.getElementsByTagName('device')].forEach(element => {
+  element.addEventListener('touchstart', touchstart);
+  element.addEventListener('mousedown', buttondown);
+  element.addEventListener('touchend', touchend);
+  element.addEventListener('mouseup', buttonup);
+});
--- flircd.py
+++ flircd.py
@@ -2,11 +2,16 @@
 
 import http
 import http.server
+import threading
 import pexpect.replwrap
 import urllib.parse
 
 
+__version__ = '0.1'
+
+
 FLIRC_UTIL: pexpect.replwrap.REPLWrapper
+FLIRC_UTIL_LOCK = threading.Lock()
 FLIRC_UTIL_COMMANDS = (
   'settings',
   'sendir',
@@ -14,7 +19,14 @@ FLIRC_UTIL_COMMANDS = (
 )
 
 
-class Handler(http.server.BaseHTTPRequestHandler):
+class Handler(http.server.SimpleHTTPRequestHandler):
+  wbufsize = -1  # http://lautaportti.wordpress.com/2011/04/01/basehttprequesthandler-wastes-tcp-packets/
+  protocol_version = 'HTTP/1.1'
+
+  def version_string(self):
+    return 'flircd/%s %s' % (
+        __version__,
+        http.server.SimpleHTTPRequestHandler.version_string(self))
 
   def do_POST(self):
     global FLIRC_UTIL
@@ -31,16 +43,18 @@ class Handler(http.server.BaseHTTPRequestHandler):
         f'--{k}={flirc_shell_escape(v)}' for k, v in flags
       ]
 
-      result = FLIRC_UTIL.run_command(' '.join(args))
+      with FLIRC_UTIL_LOCK:
+        result = FLIRC_UTIL.run_command(' '.join(args))
       self.send_response(200, 'OK')
       self.send_header('Content-Length', str(len(result)))
       self.end_headers()
       self.wfile.write(result.encode('utf8'))
       return
     elif cmd == 'restart':
-      FLIRC_UTIL.child.sendline('exit')
-      FLIRC_UTIL.child.close()
-      FLIRC_UTIL = pexpect.replwrap.REPLWrapper('flirc_util shell', 'flirc_util $', None)
+      with FLIRC_UTIL_LOCK:
+        FLIRC_UTIL.child.sendline('exit')
+        FLIRC_UTIL.child.close()
+        FLIRC_UTIL = pexpect.replwrap.REPLWrapper('flirc_util shell', 'flirc_util $', None)
       self.send_response(204, 'No Content')
       self.send_header('Content-Length', '0')
       self.end_headers()
@@ -48,7 +62,10 @@ class Handler(http.server.BaseHTTPRequestHandler):
 
     self.send_error(404)
 
+  def list_directory(self, path):
+      self.send_error(404, 'No permission to list directory')
+
 
 if __name__ == '__main__':
   FLIRC_UTIL = pexpect.replwrap.REPLWrapper('flirc_util shell', 'flirc_util $', None)
-  http.server.HTTPServer(('', 8000), Handler).serve_forever()
+  http.server.ThreadingHTTPServer(('', 8000), Handler).serve_forever()
